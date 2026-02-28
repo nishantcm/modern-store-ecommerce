05ded10549056ca2405bffce8a9364185c501844
nishantcm <cmnishantwork@gmail.com>
2026-02-28 15:59:53 +0530

    1.3 Updated the SearchBar.jsx file with width of the sidebar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar() {
       <input
         type="text"
         placeholder="Search products..."
-        className="pl-10 border border-gray-300 rounded-lg px-4 py-2 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+        className="pl-10 border border-gray-300 rounded-lg px-4 py-2 w-150 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
       />
       <span className="absolute left-3 top-2 text-gray-400">
         <i className="fa-brands fa-sistrix text-black"></i>
