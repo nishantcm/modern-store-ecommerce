d2a311408e07476f287275927d7b65cc98a9488f
nishantcm <cmnishantwork@gmail.com>
2026-03-04 17:21:55 +0530

    1.2 Updated the src/components/Header.jsx file with onClick function related to SignIn button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+"use client"
+
 import SearchBar from "./SearchBar";
 
-export default function Header() {
+export default function Header( { onOpenAuth }) {
   return (
     <header>
       <div className="grid grid-cols-12 gap-2 justify-center bg-white-500 text-white p-6 rounded-lg">
@@ -24,8 +26,8 @@ export default function Header() {
           <div className="bg-white-900 hover:bg-gray-100 text-gray-600 rounded-lg p-2 transition duration-300 flex items-center">
             <i className="fa-solid fa-cart-shopping fa-xl"></i>
           </div>
-          <div className="">
-            <button type="submit" className="bg-white-900 hover:bg-gray-100 text-gray-600 rounded-lg p-2 px-3 transition duration-300 text-[14px] font-medium">Sign In</button>
+          <div>
+            <button onClick={onOpenAuth} className="bg-white-900 hover:bg-gray-100 text-gray-600 rounded-lg p-2 px-3 transition duration-300 text-[14px] font-medium">Sign In</button>
           </div>
           <div className="">
             <button type="submit" className="bg-blue-800 hover:bg-blue-700 transiton duration-200 rounded-lg p-2 px-3 text-[14px] font-medium">Sign Up</button>
