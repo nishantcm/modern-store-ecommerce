7a2504e4cbf0b070c9128d87c4292c52df2a4a02
cmnishant <cmnishantwork@gmail.com>
2026-03-08 01:29:58 +0530

    Updated the src/app/api/signup/route.js file for fix related to server error

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -3,27 +3,44 @@ import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
 export async function POST(req) {
+  try {
 
-  const { name, email, password } = await req.json();
+    const { name, email, password } = await req.json();
 
-  await connectDB();
+    await connectDB();
 
-  const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
 
-  if (existingUser) {
-    return Response.json({ message: "User already exists" });
+    if (existingUser) {
+      return Response.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    return Response.json(
+      {
+        message: "User created successfully",
+        user
+      },
+      { status: 201 }
+    );
+
+  } catch (error) {
+
+    console.error("Signup API Error:", error);
+
+    return Response.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
   }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword
-  });
-
-  return Response.json({
-    message: "User created",
-    user
-  });
 }
