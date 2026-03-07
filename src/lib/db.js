a09f20dfb0354336a4266aba1a33b31eb99a81e9
cmnishant <cmnishantwork@gmail.com>
2026-03-08 01:31:26 +0530

    Updated the src/lib/db.js file with minnor changes

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    if (mongoose.connections[0].readyState) return;
-    await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("Database connected");
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  await mongoose.connect(process.env.MONGODB_URI);
+
+  console.log("Database connected");
 }
