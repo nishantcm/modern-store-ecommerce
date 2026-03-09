54f21e43f0110e2d0b3ce61127ea5c1470e7db6a
cmnishant <cmnishantwork@gmail.com>
2026-03-10 01:41:18 +0530

    Updated the src/components/auth/AuthModal.jsx for the onLogin user data

--- src/components/auth/AuthModal.jsx
+++ src/components/auth/AuthModal.jsx
@@ -4,35 +4,49 @@ import { useState, useEffect } from "react";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 
-export default function AuthModal({ isOpen, onClose, defaultMode }) {
+export default function AuthModal({ isOpen, onClose, defaultMode, onLogin }) {
+
   const [authMode, setAuthMode] = useState(defaultMode);
 
   useEffect(() => {
     if (isOpen) {
-        setAuthMode(defaultMode);
+      setAuthMode(defaultMode);
     }
   }, [isOpen, defaultMode]);
 
-//   Prevent rendering when closed
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-        <div className="bg-white p-6 rounded-xl w-[400px] relative">
-            <button
-                onClick={onClose}
-                className="absolute top-2 right-3 text-gray-500"
-            >
-                ✕
-            </button>
 
-            {/* Switch Based on Mode */}
-            {authMode === "signin" ? (
-                <SignIn switchToSignUp={() => setAuthMode("signup")} />
-            ) : (
-                <SignUp switchToSignIn={() => setAuthMode("signin")} />
-            )}
-        </div>
+      <div className="bg-white p-6 rounded-xl w-[400px] relative">
+
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-3 text-gray-500"
+        >
+          ✕
+        </button>
+
+        {authMode === "signin" ? (
+          <SignIn
+            switchToSignUp={() => setAuthMode("signup")}
+            onLogin={(user) => {
+              onLogin(user);
+              onClose();
+            }}
+          />
+        ) : (
+          <SignUp
+            switchToSignIn={() => setAuthMode("signin")}
+            onLogin={(user) => {
+              onLogin(user);
+              onClose();
+            }}
+          />
+        )}
+
+      </div>
     </div>
   );
 }
