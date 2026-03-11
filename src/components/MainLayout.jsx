6b7fbf6b4b5fc5ad8d4e1a411ace6c0d78bb73aa
nishantcm <cmnishantwork@gmail.com>
2026-03-11 17:35:16 +0530

    Updated the src/components/MainLayout.jsx to import sidebar

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 import AuthModal from "./auth/AuthModal";
 
 export default function MainLayout({ children }) {
@@ -10,22 +11,30 @@ export default function MainLayout({ children }) {
 
   return (
     <>
-        <Header 
-          onOpenSignIn={() => {
-            setAuthMode("signin");
-            setIsAuthOpen(true);
-          }} 
-          onOpenSignUp={() => {
-            setAuthMode("signup");
-            setIsAuthOpen(true);
-          }}
-        />
-        <AuthModal
-            isOpen={isAuthOpen}
-            onClose={() => setIsAuthOpen(false)}
-            defaultMode={authMode}
-        />
-        {children}
+      <Header
+        onOpenSignIn={() => {
+          setAuthMode("signin");
+          setIsAuthOpen(true);
+        }}
+        onOpenSignUp={() => {
+          setAuthMode("signup");
+          setIsAuthOpen(true);
+        }}
+      />
+
+      <div className="flex">
+        <Sidebar />
+
+        <main className="flex-1 ml-64 p-6">
+          {children}
+        </main>
+      </div>
+
+      <AuthModal
+        isOpen={isAuthOpen}
+        onClose={() => setIsAuthOpen(false)}
+        defaultMode={authMode}
+      />
     </>
   );
 }
