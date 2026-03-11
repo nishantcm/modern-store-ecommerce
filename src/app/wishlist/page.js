ee00378e3c254fde1776606b12bb5ae3938bfb94
cmnishant <cmnishantwork@gmail.com>
2026-03-12 00:01:38 +0530

    Created the src/app/wishlist/page.js file

---
+++ src/app/wishlist/page.js
@@ -0,0 +1,18 @@
+import WishlistItem from "@/components/Wishlist/WishlistItem";
+
+export default function WishlistPage() {
+
+  const wishlist = [];
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Your Wishlist</h1>
+
+      <div className="mt-6 space-y-4">
+        {wishlist.map((item) => (
+          <WishlistItem key={item._id} product={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
