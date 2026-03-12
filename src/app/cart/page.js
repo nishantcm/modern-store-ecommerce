9f645a19c5ccbac9286211cdca84632f279ab87f
cmnishant <cmnishantwork@gmail.com>
2026-03-12 23:59:45 +0530

    Created the src/app/cart/page.js file

---
+++ src/app/cart/page.js
@@ -0,0 +1,18 @@
+import CartItem from "@/components/Cart/CartItem";
+
+export default function CartPage() {
+
+  const cart = [];
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Your Cart</h1>
+
+      <div className="mt-6 space-y-4">
+        {cart.map((item) => (
+          <CartItem key={item._id} product={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
