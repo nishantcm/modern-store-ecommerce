75797884020ce89cf6fc86f1fe914449c76b398e
cmnishant <cmnishantwork@gmail.com>
2026-03-16 23:57:25 +0530

    Updated the src/app/cart/page.js file.

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,17 +1,47 @@
 import CartItem from "@/components/cart/CartItem";
+import { FaRegHeart } from "react-icons/fa6";
+import { IoCartSharp } from "react-icons/io5";
+
 
 export default function CartPage() {
 
   const cart = [];
 
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Your Cart</h1>
-
-      <div className="mt-6 space-y-4">
-        {cart.map((item) => (
-          <CartItem key={item._id} product={item} />
-        ))}
+    <div>
+      <div className="">
+        <button className="text-blue-600 p-2 rounded-md hover:bg-blue-100 transition">
+          <i className="fa-solid fa-arrow-left text-blue-600 mx-2"></i>
+          Continue Shopping
+        </button>
+      </div>
+      <h1 className="font-extrabold text-3xl pt-5">Shopping Cart</h1>
+      <div className="rounded-md px-3 py-5 border mt-3">
+        <div className="flex justify-between">
+          <div className="flex">
+            <div className="flex items-center justify-center">
+              <FaRegHeart className="text-red-600 text-lg me-2" />
+            </div>
+            <div className="">
+              <h3 className="text-xl font-bold">Wishlist (0 items)</h3>
+              <p className="text-sm font-light">You have 0 item saved for later</p>
+            </div>
+          </div>
+          <div className="">
+            <button className="bg-blue-800 p-2 rounded-md hover:bg-blue-700 text-white text-sm font-bold"><IoCartSharp /> Add All to Cart</button>
+          </div>
+        </div>
+        
+      </div>
+      <div className="flex justify-center">
+        <div className="p-6 ">
+          
+          <div className="mt-6 space-y-4">
+            {cart.map((item) => (
+              <CartItem key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
