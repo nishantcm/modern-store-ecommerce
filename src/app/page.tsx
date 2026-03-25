f370eb4ce4423d5d379d90a37119f2c9432c3971
cmnishant <cmnishantwork@gmail.com>
2026-03-25 23:38:47 +0530

    Updated the src/app/page.tsx file for import ProductsPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-
+import ProductsPage from "./products/page";
 
 export default function Home() {
   return (
     <>
       <main>
         <h2></h2>
+        <ProductsPage />
       </main>
     </>
   );
